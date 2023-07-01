0695d6496c622f112668ccc840d4c5dc4e69d8d7
Mohith-Kumar28 <83702382+Mohith-Kumar28@users.noreply.github.com>
2023-07-01 15:10:37 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,20 @@
+// func1
+function add(a, b, c, d) {
+    return a + b + c + d;
+  }
+  
+  // func2
+  function sub(a, b, c, d) {
+    return a - b - c - d;
+  }
+  
+  // func3
+  function mult(a, b, c, d) {
+    return a * b * c * d;
+  }
+  
+  // func4
+  function div(a, b, c, d) {
+    return a / b / c / d;
+  }
+  
